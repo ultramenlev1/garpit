0e0ee75713ad2b3d05b2c0887747429ca51d6c7a
ultramenlev1 <102772007+ultramenlev1@users.noreply.github.com>
2023-05-03 20:45:50 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # garpit
 # garpit
+# garpit
--- modelss.js
+++ modelss.js
@@ -1,5 +1,5 @@
 
-async function asyncLoadModel() {
+export async function asyncLoadModel() {
     model_url = "https://raw.githubusercontent.com/ultramenlev1/tensorflow-fire-detec/main/model.json";
 model = await tf.loadGraphModel(model_url);
 return model
